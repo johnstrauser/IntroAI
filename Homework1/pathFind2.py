1bdfdfa5f108ed04e3ec3884ea4b5a415f9d5948
Xuanang Wang <xawang47@gmail.com>
2019-10-12 00:37:00 -0400

    Fixed some bugs, but there are still some

--- Homework1/pathFind2.py
+++ Homework1/pathFind2.py
@@ -12,9 +12,9 @@ class Node():
         self.h = 0
         self.f = 0
 
+    # def __eq__(self, other):
+    # return self.position == other.position
 
-#   def __eq__(self, other):
-#       return self.position == other.position
 
 def getInsertLoc(list, f):
     # print("In the IL length of OL is", len(list))
@@ -115,6 +115,7 @@ def updatePath(agentBoard, agentLoc, targetLoc):
 
 
 def findPath(open_list, closed_list, agent_node, target_node, fullBoard, agentBoard):
+    firstIn = 1
     if agent_node.position == target_node.position:
         return 0
 
@@ -127,70 +128,82 @@ def findPath(open_list, closed_list, agent_node, target_node, fullBoard, agentBo
 
     # calculate the f value for the start(current) node
     current_node.f = current_node.g + current_node.h
+    fake_open_list = []
 
     while current_node.position != target_node.position:
-        # print("currentnode position ", current_node.position)
 
         for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
             this_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            print(this_position)
-
             # if this position [a, b] in the agentboard is not blocked
             # and is not in closed list
             # and is not in open list
             if agentBoard[this_position[0]][this_position[1]] != "X" \
                     and this_position not in closed_list \
-                    and this_position not in open_list:
+                    and this_position not in fake_open_list:
+                print(this_position)
                 # create node for this position
                 thisP_node = Node(current_node, this_position)
                 thisP_node.g = current_node.g + 1
                 thisP_node.h = abs(thisP_node.position[0] - target_node.position[0]) \
                                + abs(thisP_node.position[1] - target_node.position[1])
                 thisP_node.f = thisP_node.g + thisP_node.h
-                # print(thisP_node.f)
-                a = getInsertLoc(open_list, thisP_node.f)
-                print("i am a ", a)
-                open_list.insert(a, [thisP_node.f, thisP_node])
-                print(open_list)
+                if (firstIn == 1):
+                    open_list.append([thisP_node.f, thisP_node])
+                    fake_open_list.append(thisP_node.position)
+                    firstIn = 0
+                    print("HI")
+                else:
+                    p = 0
+                    while (p < (len(open_list))):
+                        a = open_list[p]
+                        if (a[0] > thisP_node.f):
+                            open_list.insert(p, [thisP_node.f, thisP_node])
+                            print("HI2")
+                            fake_open_list.append(thisP_node.position)
+                            p = p + 1
+                            break
+                        p = p + 1
+                    if p == 0:
+                        open_list.append([thisP_node.f, thisP_node])
+                        fake_open_list.append(thisP_node.position)
+                        print("HI1")
 
-                """" this is for update f value, might have more bug in it ...
-                k = 0
-                # traverse the whole open_list to see whether to update f value
-                for i in open_list:
-                    h = []
-                    h = i
+                    # traverse the whole open_list to see whether to update f value
+                    """for i in open_list:
+                        h = []
+                        h = i
 
                     # if this position is in the open_list
-                    if h[1].position == this_position:
-                        k = 1
+                        if h[1].position == this_position:
 
-                    if (k == 1):
-                        if h[0] > thisP_node.f:
-                            open_list.remove([thisP_node.f, thisP_node])
-                            h[0] = thisP_node.f
-                            open_list.insert(getInsertLoc(open_list, thisP_node.f), [thisP_node.f, thisP_node])
-                            h[1].parant = current_node
-                    else:
-                        # add this node to open_list
-                        # print(open_list)
-                        # print(thisP_node.f)
-                        # heapq.heappush(open_list, (thisP_node.f, thisP_node))
-                        open_list.insert(getInsertLoc(open_list, thisP_node.f), [thisP_node.f, thisP_node])"""""
+                            # if the f value which previous calculated is greater than the current one,
+                            # update the f value
+                            if h[0] > thisP_node.f:
+                                h[0] = thisP_node.f
+
+                        # sort the open_list because we changed the f value
+                        heapq.heapify(open_list)
+
+                        # set parent to be the current_node
+                        h[1].parent = current_node
+
+                        # if not, add this node into the open_list
+                        else:
+                            heapq.heappush(open_list, [thisP_node.f, thisP_node])"""
         # end of for loop, add current node into closed list
-        closed_list.append(current_node)
+        print(open_list)
+        closed_list.append(current_node.position)
+
         # print path on the agent board
         y = current_node.position[0]
         x = current_node.position[1]
         agentBoard[y][x] = "+"
+
         # pop from open_list(always the lowest f value because it's a priory queue)
-        print(len(open_list))
-        if current_node.position != target_node.position:
-            sth = []
-            sth = open_list.pop(0)
-            current_node = sth[1]
-        else:
-            return agentBoard
+
+        sth = []
+        sth = open_list.pop(0)
+        current_node = sth[1]
 
     return agentBoard
 
