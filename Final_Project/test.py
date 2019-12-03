7f09ead775a8d343678d357b6a662354e3f61a95
Xuanang Wang <xawang47@gmail.com>
2019-12-03 11:33:53 -0500

    Update test.py and put in files needed

--- Final_Project/test.py
+++ Final_Project/test.py
@@ -1,13 +1,48 @@
 # load the file, store the first image into the 'image' list
 image = []
-f = open("facedatatrain.txt")
+f = open("facedatatrain")
 for x, line in enumerate(f):
-    if x >= 2 and x <= 68:
+    if x >= 0 and x <= 69:
         image.append(line)
 f.close()
 
+# print(image)
+
 # check number of '#' in a specific region
-length = len(image[0])
-for i in range(length):
-    if image[0][i] == '#':
-        print("yes")
+#length = len(image[0])
+# for i in range(length):
+# if image[0][i] == '#':
+#print(length)
+#print(image[2])
+#print(image[2][30])
+
+# count number of '#' for a single image, spliting by each o
+# o is the small region
+num_of_x = 6
+num_of_y = 7
+x = 10
+y = 10
+num_of_o = num_of_x * num_of_y
+g = []
+index = 0
+
+
+for k in range(num_of_o - 1): # initial all value in the list to be zero
+    g.append(0)
+
+print(len(g))
+
+# range(5): 0-4; range(6,10): 6-9
+for i in range(60):
+    for j in range(70):
+        index = ((j/y) * num_of_x) + (i/x)
+        print(index)
+        print(j, i)
+        if image[j][i] == '#':
+            g[index] = g[index] + 1
+
+
+
+
+
+# we want input[the ith ][]
